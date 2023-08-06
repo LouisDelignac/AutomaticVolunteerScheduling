d38f19c29c9a950abe513fef8f749350425e3005
Louis Delignac <louis.dlgnc@gmail.com>
2023-08-06 17:04:22 +0200

    Repository reorganization

--- Main.py
+++ Main.py
@@ -2,5 +2,7 @@ from data.Volunteer import Volunteer
 from data.Division import Division
 from data.data import data_to_volunteers, data_to_divisions
 
-# volunteers = data_to_volunteers('data/volunteers.csv')
-# divisions = data_to_divisions('data/divisions.csv')
+volunteers = data_to_volunteers('ressources/volunteers.csv')
+divisions = data_to_divisions('ressources/divisions.csv')
+
+print(divisions[0])
